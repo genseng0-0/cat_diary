8d8acc4a138e217b646caee9731a12017e7fdcdb
izazha <k.izazha@cron.ooo>
2022-04-11 22:25:11 +0300

    registration screen

---
+++ app/src/main/java/com/applycation/cat_diary/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.applycation.cat_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}

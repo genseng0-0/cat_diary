46b3f41f858ef11d3feecd4aad864751b105ee51
izazha <k.izazha@cron.ooo>
2022-04-17 23:46:01 +0300

    registration screen

--- app/src/main/java/com/applycation/cat_diary/MainActivity.kt
+++ app/src/main/java/com/applycation/cat_diary/MainActivity.kt
@@ -4,14 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val vLog: EditText = findViewById(R.id.vLog)
+        val vPass: EditText = findViewById(R.id.vPass)
         val buttonSingIn: Button = findViewById(R.id.sing_in)
-        buttonSingIn.setOnClickListener { moveToNextScreen() }
+
+
+        buttonSingIn.setOnClickListener {
+            if (!(!(vLog.text.isNotEmpty() && vPass.text.isNotEmpty()) ||
+                        (vLog.text.length < 5)) && (vPass.text.length > 5)){ moveToNextScreen() }
+        }
+
+
+
     }
 
     private fun moveToNextScreen() {
--- app/src/main/java/com/applycation/cat_diary/SecondActivity.kt
+++ app/src/main/java/com/applycation/cat_diary/SecondActivity.kt
@@ -2,10 +2,19 @@ package com.applycation.cat_diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val secondText: TextView = findViewById(R.id.second_text)
+        val text: EditText = findViewById(R.id.vLog)
+
+
+        secondText.text = text.toString()   //я без понятия как это сделать
     }
 }
